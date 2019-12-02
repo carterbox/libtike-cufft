4b92dcac2dd9119bbda5abb0f0a4c49c7ea9b6be
Daniel Ching <carterbox@users.noreply.github.com>
2019-12-02 09:25:49 -0600

    TST: Add simple line integral test

--- tests/test_tomo.py
+++ tests/test_tomo.py
@@ -59,6 +59,25 @@ class TestTomoSolver(unittest.TestCase):
                   f"=? {t2.real:06f}{t2.imag:+06f}j")
             np.testing.assert_allclose(t1, t2, atol=1e-6)
 
+    def test_simple_interals(self):
+        """Check that the fwd tomo operator sums correctly at 0 and PI/2.
+
+        When we project at angles 0 and PI/2, the foward operator should be the
+        same as taking the sum over the object array along each axis.
+        """
+        theta = np.array([0, np.pi/2, np.pi, -np.pi/2], dtype='float32')
+        original = np.zeros((1, 128, 128), dtype='complex64')
+        original[0, 64, :] += 1
+        original[0, :, 64] += 1j
+        with pt.TomoCuFFT(theta, len(theta), 1, 128, 128 / 2) as slv:
+            data = slv.fwd_tomo_batch(original)
+        data1 = np.sum(original, axis=1)
+        data2 = np.sum(original, axis=2)
+        np.testing.assert_allclose(data[0], data1, atol=1e-6)
+        np.testing.assert_allclose(data[1], data2, atol=1e-6)
+        np.testing.assert_allclose(data[2], data1, atol=1e-6)
+        np.testing.assert_allclose(data[3], data2, atol=1e-6)
+
 
 if __name__ == '__main__':
     unittest.main()
