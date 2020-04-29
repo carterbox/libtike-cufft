1884ef6bc99670b82eee0b7a319473280a9ec352
Daniel Ching <carterbox@users.noreply.github.com>
2020-04-29 11:36:47 -0500

    BLD: Update version and add entry points

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='libtike-cupy',
     author='Viktor Nikitin, Daniel Ching',
-    version='0.8.0',
+    version='0.9.1',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src', include=['libtike.*']),
     include_package_data=True,
     entry_points={
         'tike.PtychoBackend': [
             'cupy = libtike.cupy:Ptycho',
+        ],
+        'tike.Propagation': [
+            'cupy = libtike.cupy:Propagation',
+        ],
+        'tike.Convolution': [
+            'cupy = libtike.cupy:Convolution',
         ],
     },
     setup_requires=[
